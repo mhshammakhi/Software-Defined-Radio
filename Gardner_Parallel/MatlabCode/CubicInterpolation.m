bb559ed81b64943ee05e9f9fcc88b3faf41bb24e
Mohammad Hasan Shammakhi <mh.shammakhi@gmail.com>
2021-10-01 13:52:14 +0330

    Move
    To Add New Version

--- Gardner_Parallel/MatlabCode/CubicInterpolation.m
+++
@@ -1,7 +0,0 @@
-function Out = CubicInterpolation(samp0,samp1,samp2,samp3,mu)
-v0 = samp1;
-v1 =  (-1/3)*samp0 + (-0.5)*samp1 + samp2 + (-1/6)*samp3;
-v2 = (0.5)*samp0 + (-1.0)*samp1 + (0.5)*samp2;
-v3 = (-1/6)*samp0 + (0.5)*samp1 + (-0.5)*samp2 + (1/6)*samp3;
-Out = v0+mu*(v1+mu*(v2+mu*v3));
-end
