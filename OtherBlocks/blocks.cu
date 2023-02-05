f547586825125f12609dcfc2850c1da9a60b508c
Nastaran Kianersi <nasyaee16@gmail.com>
2023-02-05 11:25:50 +0330

    Cleaned up the blocks a little bit

---
+++ OtherBlocks/blocks.cu
@@ -0,0 +1,54 @@
+#include "blocks.cuh"
+
+__constant__ float c_bbFilterCoeffs[1024];
+
+__global__
+void Baseband(cuComplex output[], const cuComplex input[], const float* freq_init,
+    const int dataLength, const float frequency)
+{
+    int i = (blockDim.x * blockIdx.x) + threadIdx.x;
+    float local_freq_init = *freq_init;
+    cuComplex local_input;
+
+    float freq, omega;
+    while (i < dataLength)
+    {
+        float freq = float(i % 10000) * frequency;
+        omega = 2.0 * M_PI * (freq - int(freq)) + local_freq_init;
+        local_input = input[i];
+        output[i].x = local_input.x * cosf(omega) + local_input.y * sinf(omega);
+        output[i].y = local_input.y * cosf(omega) - local_input.x * sinf(omega);
+        i += blockDim.x * gridDim.x;
+    }
+}
+
+__global__
+void Baseband_Update_State(float* freq_init, const float frequency, const int dataLength) {
+    float freqLast = float(dataLength % 10000) * frequency;
+    *freq_init += 2.0 * M_PI * (freqLast - int(freqLast));
+}
+
+void setBBFilterCoeffsConstMem(const float cpu_array[], const int& filterLen) {
+    cudaMemcpyToSymbolAsync(c_bbFilterCoeffs, cpu_array, filterLen * sizeof(float), 0, cudaMemcpyHostToDevice);
+}
+
+__global__
+void BasebandFilter(cuComplex output[], const cuComplex input[],
+    const int filterLength, const int dataLength) {
+
+    int i = blockDim.x * blockIdx.x + threadIdx.x;
+    cuComplex sum;
+    while (i < dataLength)
+    {
+        sum = { 0.f, 0.f };
+
+        for (int j = 0; j < filterLength; j++)
+        {
+            sum.x += c_bbFilterCoeffs[j] * input[i - j + filterLength - 1].x;
+            sum.y += c_bbFilterCoeffs[j] * input[i - j + filterLength - 1].y;
+        }
+
+        output[i] = sum;
+        i += (blockDim.x * gridDim.x);
+    }
+}
